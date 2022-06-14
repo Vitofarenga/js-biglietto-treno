2657123f75f0a5a97744bf0c1038973ebd16fba0
Vito Farenga <vitofarenga@libero.it>
2022-06-14 16:27:56 +0200

    base structure

---
+++ main.js
@@ -0,0 +1,16 @@
+// età
+let userAge = prompt("inserisci la tua età");
+console.log(userAge);
+
+document.getElementById("user-age").innerHTML = userAge;
+
+// distanza da percorrere
+let userDistance = prompt("inserisci la distanza da percorrere");
+console.log(userDistance);
+
+document.getElementById("user-distance").innerHTML = userDistance;
+
+// calcolo
+if ( userAge < 18 ) {
+    
+}
